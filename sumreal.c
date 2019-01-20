30c65690d994180b8619c916c0bd3ca43e1212a3
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-01-20 07:59:18 +0530

    sumreal.c

---
+++ sumreal.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i,sum=0;
+    clrscr();
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        sum=sum+i;
+    }
+    printf("%d",sum);
+    getch();
+}
