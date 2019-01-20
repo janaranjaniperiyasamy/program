da44c68cf1671f473226c9f17a9f30a1aa8a40d9
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-01-20 07:55:41 +0530

    hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i;
+    clrscr();
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        printf("Hello\n");
+    }
+    getch();
+}
