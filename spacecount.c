679dae1724bfa05419c3960176c7fed5d7657a68
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-01-29 12:09:27 +0530

    spacecount.c

---
+++ spacecount.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    int i,c=0;
+    scanf("%[^\n]s",a);
+    for(i=0;a[i]!=NULL;i++)
+    {
+        if(a[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
