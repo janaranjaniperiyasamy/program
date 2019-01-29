eac7c57f7833abdc1e77b8429e28f56d6a3d020b
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-01-29 12:15:27 +0530

    numericcount.c

---
+++ numericcount.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    int i,c=0;
+    scanf("%[^\n]s",a);
+    for(i=0;a[i]!=NULL;i++)
+    {
+        if((a[i]>='0') && (a[i]<='9'))
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
