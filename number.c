7c59ee64d4585a5f58e10913652e1d5ea2690707
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-01-31 20:31:06 +0530

    number.c

---
+++ number.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==1)
+    {
+        printf("One");
+    }
+    if(a==2)
+    {
+        printf("Two");
+    }
+    if(a==3)
+    {
+        printf("Three");
+    }if(a==4)
+    {
+        printf("Four");
+    }
+
+if(a==5)
+    {
+        printf("Five");
+    }
+
+if(a==6)
+    {
+        printf("Six");
+    }
+if(a==7)
+    {
+        printf("Seven");
+    }
+if(a==8)
+    {
+        printf("Eight");
+    }
+if(a==9)
+    {
+        printf("Nine");
+    }
+if(a==10)
+    {
+        printf("Ten");
+    }
+
+
+
+    return 0;
+}
