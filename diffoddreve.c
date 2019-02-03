38ab004ca920c445d7d43534b89c9f13c853e82d
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-02-03 18:22:13 +0530

    diffoddreve.c

---
+++ diffoddreve.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,k,c;
+    scanf("%d %d",&n,&k);
+    c=n-k;
+        if(c%2==0)
+        {
+        printf("even");
+        }
+        else
+        {
+            printf("odd");
+            
+        }
+    return 0;
+}
