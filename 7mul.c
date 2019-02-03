d79a4ddad77770ad93341f7a918d2b2f430b5854
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-02-03 18:19:15 +0530

    7mul.c

---
+++ 7mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+        if(n%7==0)
+        {
+        printf("yes");
+        }
+        else
+        {
+            printf("no");
+            
+        }
+    return 0;
+}
