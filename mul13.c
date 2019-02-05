a9bb9a362d0ae34c9e152d1ad3aa768069875531
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-02-05 19:47:52 +0530

    mul13.c

---
+++ mul13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    scanf("%d",&a);
+      if(a%13==0)
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    
+}
