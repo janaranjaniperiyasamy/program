b73106da1738fae858d1c118e95bc89123605f67
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-02-17 15:04:23 +0530

    divby2.c

---
+++ divby2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a>b?a:b;
+    while(1)
+    {
+        if(c%a==0 && c%b==0)
+        {
+            printf("%d",c);
+            break;
+        }
+        else
+        {
+            c++;
+        }
+    }
+    return 0;
+}
