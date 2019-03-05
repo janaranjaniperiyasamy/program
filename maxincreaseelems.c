ff93177f9a53e4792b97a63b8a3dc1601f933a71
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-05 20:28:23 +0530

    maxincreaseelems.c

---
+++ maxincreaseelems.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[1000],i,c=1,c1=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    if(a[i]<a[i+1])
+    {
+        c++;
+    }
+    else
+    {
+        if(c1<c)
+        {
+            c1=c;
+            c=1;
+        }
+    }
+    }
+    printf("%d",c1);
+    return 0;
+}
