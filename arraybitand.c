06d2875a59ccf9af105c169a6b059af8560f4c50
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-10 15:50:07 +0530

    arraybitand.c

---
+++ arraybitand.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a[1000],n,i,j,r,max=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            r=(a[i]&a[j]);
+            if(max<r)
+            {
+                max=r;
+            }
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
