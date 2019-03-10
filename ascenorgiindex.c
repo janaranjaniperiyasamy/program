9ed72799928080ea273d3b5c87fbdf722eb8cf84
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-10 14:46:48 +0530

    ascenorgiindex.c

---
+++ ascenorgiindex.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+   int a[1000],b[10000],n,temp,i,j;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+       b[i]=a[i];
+   }
+   for(i=0;i<n;i++)
+  {
+      for(j=i;j<n;j++)
+      {
+          if(a[i]>a[j])
+          {
+              temp=a[i];
+              a[i]=a[j];
+              a[j]=temp;
+            }
+      }
+  }
+ 
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<n;j++)
+       {
+       if(a[i]==b[j])
+       {
+           printf("%d ",j+1);
+       }
+   }
+   }
+    return 0;
+}
