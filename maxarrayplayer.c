0e2a17b38fc49c7b2277a79250fe9c92ab30d5f2
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-11 20:04:29 +0530

    maxarrayplayer.c

---
+++ maxarrayplayer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10000],i,n,max=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
