5bf662448eafe40669564b84c8d7d8ac9331fd30
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-17 17:20:36 +0530

    138.c

---
+++ 138.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int b,i=1,j,r=0;
+    scanf("%d",&b);
+    for(j=0;j<=b/2;j++)
+    {
+    i=i*2;
+    if(i==b)
+    {
+    r=1;
+    }}
+    if(r==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
