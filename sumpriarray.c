fd0ce091835a49b3e135e4895c6449f020b32c05
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-17 16:21:53 +0530

    sumpriarray.c

---
+++ sumpriarray.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int a[10000],i,n,j,sum;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            sum=0;
+            for(j=0;j<=i;j++)
+            {
+                sum=sum+a[j];
+            }
+            printf("%d ",sum);
+        }
+        else
+        {
+            printf("%d ",a[i]);
+        }
+    }
+    return 0;
+}
