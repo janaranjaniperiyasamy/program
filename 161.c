b874bff931496bf107b4fc0fd7e26fd3cada1fbb
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-23 14:03:01 +0530

    161.c

---
+++ 161.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100][1000],b[1000];
+    int i,j,n,s,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        strcpy(b,a[i]);
+        s=0;
+        for(j=0;b[j]!=NULL;j++)
+        {
+            if((b[j]=='a'||b[j]=='e'||b[j]=='i'||b[j]=='o'||b[j]=='u') || (b[j]=='A'||b[j]=='E'||b[j]=='I'||b[j]=='O'||b[j]=='U'))
+            {
+                s=1;
+            }
+        }
+        if(s==1)
+        {
+            c++;
+        }
+    }
+    if(c==n)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
