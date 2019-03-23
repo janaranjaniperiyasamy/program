6123b58ff41d75041d04104a037bab91b97e45f4
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-23 13:52:09 +0530

    160.c

---
+++ 160.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    long int a,b,c,i=0,r,a1[100000],j,c1=0;
+    scanf("%ld %ld",&a,&b);
+    c=a|b;
+    while(c)
+    {
+        r=c%2;
+        c=c/2;
+        a1[i]=r;
+        i++;
+    }
+    for(j=0;j<i;j++)
+    {
+        if(a1[j]==1)
+        {
+            c1++;
+        }
+    }
+    printf("%ld",c1);
+    return 0;
+}
