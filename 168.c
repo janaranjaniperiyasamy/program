17603df27cf9e97b296210e7e8efab48d6ba2059
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-03-23 15:42:02 +0530

    168.c

---
+++ 168.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100000];
+    int i,c=0;
+    scanf("%s",a);
+    for(i=0;a[i]!=NULL;i++)
+    {
+        if(a[i]>='0' && a[i]<='9')
+        {
+            c=a[i]-48;
+            while(c)
+            {
+                printf("%c",a[i-1]);
+                c--;
+            }
+        }
+    }
+
+    return 0;
+}
